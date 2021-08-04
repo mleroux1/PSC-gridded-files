3cbe5784cd9d85e95bc1efd82cc47a33ab816800
mleroux1 <86771843+mleroux1@users.noreply.github.com>
2021-08-04 10:23:02 +0200

    Update and rename DenitriMonth.py to Denitrification_Month.py

--- Fonctions/Denitrification_Month.py
+++ Fonctions/Denitrification_Month.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
-import glob
-from Fonctions import Denitri
-from Fonctions import Nbinftropo
+
+## Calcul de la dénitrification mensuel à partir des fichiers hdf journaliers
+
+# Importation des modules et fonctions utiles
+from Fonctions import Denitrification
+from Fonctions import FctProf
 import numpy as np 
 import xarray as xr
+import glob
 
-def denitrification_month(year, month, latsize=2, lonsize=15):
+def denitrification_month(year, month, latsize, lonsize):
     filesmonth=sorted(glob.glob('/bdd/CALIPSO/Lidar_L2/LID_L2_PSCMask-Prov-V1-00/%04d/%02d/*.hdf' % (year, month)))
 
     if len(filesmonth)==0:
@@ -18,15 +22,13 @@ def denitrification_month(year, month, latsize=2, lonsize=15):
     nbTOT=np.zeros([len(latbin),len(longbin)])
 
     for k in range(len(filesmonth)):
-        
         if k % 2 == 0:
             print('file #%02d/%02d' % (k, len(filesmonth)))
         
-        y=Denitri.denitrification(filesmonth[k],2,15)
+        y=Denitrification.denitri(filesmonth[k],latsize,lonsize)
         if y is None:
             continue
 
-        nbNAT=nbNAT+y    #Attention si on somme, il va falloir diviser par le nombre de jours donc de fichiers !
-
+        nbNAT=nbNAT+y    #Attention si on somme, il va falloir diviser par le nombre de jours (de fichiers) pour normaliser.
  
     return(nbNAT)
